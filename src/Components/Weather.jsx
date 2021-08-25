5fe313b4be232afffe2003cdc0e294dba4f2330d
Teo Theodoridis <teo.godgift4@gmail.com>
2021-08-25 17:13:45 +0300

    Hard coded values

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const Weather = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>London</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny"></i>
+                </h5>
+                <h1 className="py-2">25&deg;</h1>
+                {minmaxTemp(19,24)}
+                <h4 className="py-3">Slow Rain</h4>
+            </div>
+        </div>
+    );
+}
+
+const minmaxTemp = (min, max) =>{
+    return(
+        <>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+        </>
+    );
+}
+
+export default Weather;
