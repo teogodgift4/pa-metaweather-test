4498701bea863ce0d3cb7d7b180936f1f83217a0
Teo Theodoridis <teo.godgift4@gmail.com>
2021-08-26 23:48:11 +0300

    Logic ready -remains styling

--- src/App.js
+++ src/App.js
@@ -1,66 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import "weather-icons/css/weather-icons.css"
-import Weather from './Components/Weather';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Form from './Components/Form';
-import { Component, useState } from 'react';
+import { Component } from 'react';
 
 export default class App extends Component{
   constructor(props){
     super(props);
-    this.state={
-      value: '',
-      // city:'New York',
-      // weather:'Heavy Clouds',
-      // wind_comp:'NNE',
-      // min_temp:0,
-      // max_temp:4,
-      // temp:3,
-      // wind_speed:5,
-      // wind_dir:17,
-      // air:1023,
-      // hum:56,
-      // visib:10.345
+    this.state={value: ''}
     }
-    //this.getWeather();
-  }
-
-//   getWeather = async(e)=>{   
-//     const api_call = await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/44418/');
-//     const response = await api_call.json();
-//     console.log(response);
-
-// }
-
-  // const [city, setCity] = useState('New York');
-  // const [weather, setWeather] = useState('Heavy Clouds');
-  // const [wind_comp, setWind_comp] = useState('NNE');
-  // const [min_temp, setMin_temp] = useState('0');
-  // const [max_temp, setMax_temp] = useState('4');
-  // const [temp, setTemp] = useState('2');
-  // const [wind_speed, setWind_speed] = useState('3');
-  // const [wind_dir, setWind_dir] = useState('17');
-  // const [air, setAir] = useState('1023');
-  // const [hum, setHum] = useState('56');
-  // const [visib, setVisib] = useState('10.034555');
-
-  render(){
+render(){
   return (
     <>
-    <Form value={this.state.value} 
-    // cityname={this.state.city}
-    // weather_val={this.state.weather}
-    // wind_val={this.state.wind_comp}
-    // min_temp_val={this.state.min_temp}
-    // max_temp_val={this.state.max_temp}
-    // temp_val={this.state.temp}
-    // wind_speed_val={this.state.wind_speed}
-    // wind_dir_val={this.state.wind_dir}
-    // air_val={this.state.air}
-    // hum_val={this.state.hum}
-    // visib_val={this.state.visib}
-    />
+    <Form value={this.state.value} />
     </>
   );
   }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Weather from './Weather';
 
 export default class Form extends React.Component {
@@ -35,17 +35,31 @@ export default class Form extends React.Component {
   
 
   handleSubmit = async(e) => {
-    alert('A name was submitted: ' + this.state.value);
+    //alert('A city was submitted: ' + this.state.value);
     e.preventDefault();
         const api_call2 = await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query='+this.state.value);
         const response2 = await api_call2.json();
         var woeid = response2[0].woeid;
-        console.log(woeid);
+        //console.log(woeid);
         const api_call = await fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/'+woeid);
         const response = await api_call.json();
-        console.log(response);
+        //console.log(response.consolidated_weather[0]);
         this.setState({
             city:this.state.value,
+            weather:response.consolidated_weather[0].weather_state_name,
+            wind_comp:response.consolidated_weather[0].wind_direction_compass,
+            min_temp:response.consolidated_weather[0].min_temp,
+            max_temp:response.consolidated_weather[0].max_temp,
+            temp:response.consolidated_weather[0].the_temp,
+            wind_speed:response.consolidated_weather[0].wind_speed,
+            wind_dir:response.consolidated_weather[0].wind_direction,
+            air:response.consolidated_weather[0].air_pressure,
+            hum:response.consolidated_weather[0].humidity,
+            visib:response.consolidated_weather[0].visibility,
+            weather_state:response.consolidated_weather[0].weather_state_abbr,
+            created:response.consolidated_weather[0].created,
+            app_date:response.consolidated_weather[0].applicable_date,
+            predict:response.consolidated_weather[0].predictability,
         });
   }
 
