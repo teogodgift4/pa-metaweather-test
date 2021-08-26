3300f055be4bed065780a2d8b76b40155f2e920d
Teo Theodoridis <teo.godgift4@gmail.com>
2021-08-26 18:47:04 +0300

    data-properties for weather component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import './App.css';
 import "weather-icons/css/weather-icons.css"
 import Weather from './Components/Weather';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Weather_fetch from './Components/Weather_fetch';
+import Form from './Components/Form';
+import { useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState('New York');
+  const [weather, setWeather] = useState('Heavy Clouds');
+  const [wind_comp, setWind_comp] = useState('NNE');
+  const [min_temp, setMin_temp] = useState('0');
+  const [max_temp, setMax_temp] = useState('4');
+  const [temp, setTemp] = useState('2');
+  const [wind_speed, setWind_speed] = useState('3');
+  const [wind_dir, setWind_dir] = useState('17');
+  const [air, setAir] = useState('1023');
+  const [hum, setHum] = useState('56');
+  const [visib, setVisib] = useState('10.034555');
+
   return (
     <>
-    <Weather/>
-    <Weather_fetch/>
+    <Form/>
+    <Weather 
+    cityname={city}
+    weather_val={weather}
+    wind_val={wind_comp}
+    min_temp_val={min_temp}
+    max_temp_val={max_temp}
+    temp_val={temp}
+    wind_speed_val={wind_speed}
+    wind_dir_val={wind_dir}
+    air_val={air}
+    hum_val={hum}
+    visib_val={visib}
+    />
     </>
   );
 }
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 
-const Weather = () => {
+const Weather = (props) => {
     return(
         <div className="container">
             <div className="cards">
-                <h1>London</h1>
+                <h1>{props.cityname}</h1>
                 <h5 className="py-4">
                     <i className="wi wi-day-sunny"></i>
                 </h5>
-                <h1 className="py-2">25&deg;</h1>
-                {minmaxTemp(19,24)}
-                <h4 className="py-3">Slow Rain</h4>
+                <h1 className="py-2">{props.temp_val}&deg;</h1>
+                <span className="px-4">{props.min_temp_val}&deg;</span>
+                <span className="px-4">{props.max_temp_val}&deg;</span>
+                <h4 className="py-3">{props.weather_val}</h4>
+                <h3 className="py-2">{props.wind_val}&deg;</h3>
+                <span className="px-4">{props.wind_speed_val}&deg;</span>
+                <span className="px-4">{props.wind_dir_val}&deg;</span>
+                <h4 className="py-3">{props.air_val}</h4>
+                <h4 className="py-3">{props.hum_val}</h4>
+                <h4 className="py-3">{props.visib_val}</h4>
             </div>
         </div>
     );
 }
 
-const minmaxTemp = (min, max) =>{
-    return(
-        <>
-        <span className="px-4">{min}&deg;</span>
-        <span className="px-4">{max}&deg;</span>
-        </>
-    );
-}
+
 
 
 export default Weather;
